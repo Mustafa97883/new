a1a39106e866589c7c0b0ebac5698e4247ec835b
Glitch (sayisal-bot-v234123123e142123432) <none>
2022-07-03 22:09:03 +0000

    🌃📕 Checkpoint ./komutlar/yardım.js:14029226/5000

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -2,12 +2,14 @@ const Strom = require("discord.js");
 const db = require("quick.db");
 const ayarlar = require("../ayarlar.json");
 const talkedRecently = new Set();
+const DBL = require('dblapi.js')
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1Njg4MzMwOTI3MDY2MzIyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjU2MTEwOTkxfQ.7Oqg1lelprL5ACm4Yh0RKREKaOTPIyQRrSjDaT7uKko', client)
 let botid = "756883309270663229";
+dbl.hasVoted(message.author.id).then(voted => {
+      if(voted) {
 
 exports.run = async (client, message, args) => {
-  const DBL = require('dblapi.js')
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1Njg4MzMwOTI3MDY2MzIyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjU2MTEwOTkxfQ.7Oqg1lelprL5ACm4Yh0RKREKaOTPIyQRrSjDaT7uKko', client)
-  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
+
 
   
   
@@ -42,11 +44,7 @@ const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1Njg4MzMwOT
 },
 
 
-   else {
-        return tools.embed(msg, ` Bu Komutu Sadece 12 Saatte Bir Oyvererek Kullanabilirsiniz Oyvermek İçin [Oyver](https://discordbots.org/bot/756883309270663229/votevote) Yazısına Tıklayarak Oyverebilirsiniz. Oy Verdiyseniz 5 Dakka Bekleyiniz`) 
-                             }
-        })
-  
+
   
   
   exports.conf = {
