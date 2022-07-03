ba632e00034c83ab2c77b21c088146d600c68554
Glitch (sayisal-bot-v234123123e142123432) <none>
2022-07-03 22:40:50 +0000

    🌌⛲️ Checkpoint ./komutlar/y-nsfw.js:14029226/9901 ./komutlar/yardım-sunucuKUR.js:14029226/1155

--- komutlar/y-nsfw.js
+++ komutlar/y-nsfw.js
@@ -1,15 +1,6 @@
 const akaneko = require("akaneko")
 const Discord = require("discord.js")
-const DBL = require('dblapi.js')
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1Njg4MzMwOTI3MDY2MzIyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjU2MTEwOTkxfQ.7Oqg1lelprL5ACm4Yh0RKREKaOTPIyQRrSjDaT7uKko', client)
-
-
-var prefix = ayarlar.prefix;
-
-dbl.hasVoted(message.author.id).then(voted => {
-      if(voted) {
-
-exports.run = async(client, bot, message, args) => {
+exports.run = async(bot, message, args) => {
     
   
   
@@ -143,11 +134,9 @@ exports.run = async(client, bot, message, args) => {
         message.channel.send(wrongarr)
 
     }
+
+
 };
-} else {
-        return tools.embed(msg,"Bu Komutu Sadece 12 Saatte Bir Oyvererek Kullanabilirsiniz Oyvermek İçin (https://top.gg/bot/756883309270663229/vote) linkine Tıklayarak Oyverebilirsiniz. Oy Verdiyseniz 5 Dakka Bekleyiniz")
-      }
-  })};
 
 exports.conf = {
     aliases: ['nsfw'],
