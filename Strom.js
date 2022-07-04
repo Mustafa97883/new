7760e7c0124dcbb839aa680897d716c097dc9826
Glitch (sayisal-bot-v234123123e142123432) <none>
2022-07-04 00:12:45 +0000

    ⛸🚄 Checkpoint ./Strom.js:14029226/1132

--- Strom.js
+++ Strom.js
@@ -1206,27 +1206,6 @@ if(TheSid === "acik") {
 
 
 
-//spamv2
-
-//---Bu Kod botun ana dosyasına atılacaktır.
-//###CodeMareFi tarafından hazırlanmıştır - - - Ekleyen //###Sadista
-
-const antispam = require("discord-anti-spam-tr");
-
-//istediğiniz yere ekleyin bot.js de
-antispam(client, {
-  uyarmaSınırı: 2, //Uyarılmadan önce aralıkta gönderilmesine izin verilen maksimum mesaj miktarı.
-  banlamaSınırı: 7, //Yasaklanmadan önce aralıkta gönderilmesine izin verilen maksimum ileti miktar.
-  aralık: 1000, // ms kullanıcılarda zaman miktarı, yasaklanmadan önce aralık değişkeninin maksimumunu gönderebilir.
-  uyarmaMesajı: "Spamı Durdur Yoksa Mutelerim.", // Uyarı mesajı, kullanıcıya hızlı gideceklerini belirten kullanıcıya gönderilir..
-  rolMesajı: "Spam için yasaklandı, başka biri var mı?", //Yasak mesaj, yasaklanmış kullanıcıyı ,Banlar
-  maxSpamUyarı: 2,//Bir kullanıcının uyarılmadan önce bir zaman dilimi içinde gönderebileceği maksimum kopya sayısı
-  maxSpamBan: 12, //Bir kullanıcının yasaklanmadan önce bir zaman diliminde gönderebildiği maksimum kopya sayısı
-  zaman: 7, // Spamdan sonraki zaman
-  rolİsimi: "spam-susturulmuş" // Spam Atan Kullanıcılar Verilecek Rol
-});
-
-
 
 
 
