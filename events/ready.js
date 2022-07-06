7f11e300b20f5c6e4b3e542f994aae1c37cd3c0b
Glitch (sayisal-bot-v234123123e142123432) <none>
2022-07-06 18:16:14 +0000

    🌴🛳 Checkpoint ./events/ready.js:14029226/63

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@
 const ayarlar = require('../ayarlar.json');
 
 module.exports = async client => {
-  var oyun = ["🔥s!güncellemeler🔥","🔥s!yardım🔥","🔥s!davet🔥","🔥s!oyver🔥","🔥s!kayıtsistemi🔥","🔥s!güncellemeler🔥","🔥s!guard🔥","🔥s!moderasyon🔥"];
+  var oyun = ["🔥s!güncellemeler🔥","🔥s!yardım🔥","🔥s!davet🔥","🔥s!oyver🔥","🔥s!eğlence🔥","🔥s!güncellemeler🔥","🔥s!guard🔥","🔥s!moderasyon🔥"];
 
   setInterval(async () => {
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
