a20adef839d279e837d3b905eb8d065bbf6e3ea1
Glitch (sayisal-bot-v234123123e142123432) <none>
2022-07-15 19:52:24 +0000

    🏰👡 Checkpoint ./komutlar/tavsiye.js:14029226/2731

--- komutlar/tavsiye.js
+++ komutlar/tavsiye.js
@@ -1,34 +1,48 @@
 const Discord = require('discord.js');
+const db = require('quick.db')
+const ayarlar = require("../ayarlar.json");
+
 
 
 exports.run = function(client, message, args) {
+    let hata = args.join(" ")
+    let şikayetlog = "826124655613575268"
+    let prefix = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix;
     let type = args.slice(0).join(' ');
-    if (type.length < 1) return message.channel.send(
-new Discord.MessageEmbed()
-.setDescription('Kullanım: s!tavsiye <Tavsiyeniz>'));
-const embed = new Discord.MessageEmbed()
-.setColor('RANDOM')
-.setDescription('Tavsiyeniz Bildirildi!')
-message.channel.send(embed)
-const embed2 = new Discord.MessageEmbed()
-.setColor("RANDOM")
-.setDescription(`**${message.author.tag}** adlı kullanıcının tavsiyesi:`)
-.addField(`Kulanıcı Bilgileri`, `Kullanıcı ID: ${message.author.id}\nKullanıcı Adı: ${message.author.username}\nKullanıcı Tagı: ${message.author.discriminator}`)
-.addField("Tavsiye", type)
-.setThumbnail(message.author.avatarURL())
-client.channels.cache.get('989518783519555674').send(embed2); // Kanal ID
+    if (type.length < 1) return message.channel.send(new Discord.MessageEmbed().setDescription(`
+\`__Hatalı Kullanım...__\`
+${hata}
+\`__Doğru Kullanım__\`
+\`${prefix}öneri <öneririniz>\`**`));
 
+const ace = new Discord.MessageEmbed()
+.setDescription(`<@${message.author.id}>\n\n Öneriniz Bildirildi! En Kısa Sürede Geri Dönüş Yapılıcakatır.\n\n Anlayışınız İçin Teşekkürler`)
+.setThumbnail(message.author.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+message.channel.send(ace)
+
+const acee = new Discord.MessageEmbed()
+.setDescription(`<@${message.author.id}> adlı kullanıcının **__Önerisi__**:`)
+.addField(`
+**Kulanıcı Bilgileri**`,
+` **__Kullanıcı ID:__** **\`${message.author.id}\`**
+**__Kullanıcı Adı:__** **\`${message.author.username}\`**
+**__Kullanıcı Tagı:__** **\`#${message.author.discriminator}\`**`)
+.addField("Kullanıcı Önerisi", type)
+.addField('önerinizi kontrol etmek için **')
+.addField("[KONTROL](https://discord.gg/6xQtMXjRsA)")
+.setThumbnail(message.author.avatarURL)
+ client.channels.cache.get(şikayetlog).send(acee);
 };
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
+  guildOnly: false, 
+  aliases: ["öner","alviyi öner"],
+  permLevel: 0 
 };
 
 exports.help = {
-  name: 'tavsiye',
-  description: 'Bot için tavsiye bildirirsiniz',
-  usage: 'tavsiye <tavsiyeniz>'
-};
+  name: 'öneri',
+  description: 'Şikayet de bulunursunuz..',
+  usage: 'şikayet <şikayet>'
+}; 
