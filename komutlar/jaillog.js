ef5ba248fab118f1768635e622c0f7988bc11bed
Glitch (sayisal-bot-v234123123e142123432) <none>
2022-07-16 18:02:20 +0000

    Rewound to commit 907727614e1e5ba41eaa8a3985c39ea9de3f638d + 0 edits

--- komutlar/jaillog.js
+++ komutlar/jaillog.js
@@ -1,21 +1,39 @@
-const Discord = require('discord.js');
-const data = require('quick.db');
+const Discord = require('discord.js')
+const db = require("quick.db");
+const database = require('quick.db');
 
-exports.run = async (client, message, args) => {
+
+exports.run = async(client, message, args) => {
 const DBL = require('dblapi.js')
 const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1Njg4MzMwOTI3MDY2MzIyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjU2MTEwOTkxfQ.7Oqg1lelprL5ACm4Yh0RKREKaOTPIyQRrSjDaT7uKko', client)
 dbl.hasVoted(message.author.id).then(voted => {
-      if(voted) {   
-  
-  const ayarlar = require('../ayarlar.json')
-const db = require('quick.db')
-let prefix = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix;
-  if(!message.member.permissions.has('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.glitch.global/d80f45da-14d1-42a7-a16d-8f3385ce86a4/c10f8dedd93c798ce1e36ad9acb26fd4.png?v=1657994146595').setImage('https://cdn.glitch.com/0c8ef551-5187-48a8-9daf-f2cc35630f21%2Fyoneticigif.gif').setTitle('Bir hata oldu!').setDescription(`• \`${prefix}jail-log \` **kullanmak için,** \`Yönetici\` **yetkisine sahip olman gerekiyor.**`));
-  if(!message.mentions.channels.first()) return message.channel.send(new Discord.MessageEmbed().setColor('#00001').setTitle('Bir hata oldu!').setDescription('Bir kanal etiketlemeyi unuttunuz.'));
-  let mentionChannel = message.mentions.channels.first();
-  data.set(`jail.log.${message.guild.id}`, mentionChannel.id);
-  message.channel.send(new Discord.MessageEmbed().setTitle('İşte bu kadar!').setDescription(`Jail sistemi başarıyla açıldı. ${mentionChannel} kanalı olarak seçtiniz.`));
+      if(voted) {  
+   var başarılı = ['**İŞTE BU!** <a:tik1:823900091143159828>', '**SÜPER!** <a:tik1:823900091143159828>', '**NASIL YAPTIN BUNU?!** <a:tik1:823900091143159828>', '**MÜKEMMEL!** <a:tik1:823900091143159828>', '**SEVDİM BUNU!** <a:tik1:823900091143159828>', '**ŞİMDİ OLDU!** <a:tik1:823900091143159828>'];
+   var x = başarılı[Math.floor(Math.random() * başarılı.length)];
 
+   var başarısız = ['**TÜH!** <a:nope:819837486741192784>', '**OLMADI BU!** <a:alarm3:823900153831620658>', '**HAY AKSİ!** <a:alarm3:823900153831620658>', '**HADİ ORADAN!** <a:alarm3:823900153831620658>', '**OLMADI YA!** <a:alarm3:823900153831620658>', '**BÖYLE OLMAZ?!** <a:alarm3:823900153831620658>', '**HADİ YA!** <a:alarm3:823900153831620658>'];
+   var x2 = başarısız[Math.floor(Math.random() * başarısız.length)];
+  
+if (!message.member.hasPermission('MANAGE_GUILD')) return message.reply(`**s!jail-kanal ayarla/sıfırla** isimli komutu kullanabilmek için \`SUNUCUYU YÖNET\` yetkisine sahip olman gerekiyor.`)
+if (!args[0]) return message.reply(`Sistemi kullanabilmek için, !sjail-kanal ayarla/sıfırla #kanal yazmalısın.`)
+   
+  
+  if (args[0] == 'ayarla') {
+  
+  let kanal = message.mentions.channels.first() || message.guild.channels.find(c => c.name === args[1].join('-'))
+  if (!kanal) return message.channel.send(x2 + ` Bir kanal etiketle.`)
+  
+  db.set(`jailkanal_${message.guild.id}`, kanal.id)
+  message.channel.send(x + ` Jail logunun tutulacağı kanal ${kanal} olarak ayarlandı.`)
+  } 
+  
+
+  if (args[0] == 'sıfırla') {
+    db.delete(`jailkanal_${message.guild.id}`)
+    message.channel.send(x + ` Jail logunun tutulduğu kanal başarıyla sıfırlandı.`)
+  }
+  
+  
 } else {
         message.channel.send(` Bu Komutu Sadece 12 Saatte Bir Oyvererek Kullanabilirsiniz Oyvermek İçin (https://top.gg/bot/756883309270663229/vote) linke Tıklayarak Oyverebilirsiniz. Oy Verdiyseniz 5 Dakka Bekleyiniz`) 
               
@@ -24,12 +42,14 @@ let prefix = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix;
       
       },
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: [],
-  permLevel: 0
-}
+ enabled: true,
+ guildOnly: false,
+ aliases: ['jailkanal'],
+ permLevel: 0
+};
 
 exports.help = {
-  name: 'jail-log'
-};
+ name: 'jail-kanal',
+ description: 'Birisi jaile atılınca hangi kanala mesaj atılacağını ayarlarsınız.',
+ usage: 'jail-kanal ayarla/sıfırla #kanal'
+}
--- komutlar/jailyetkilirol.js
+++ komutlar/jailyetkilirol.js
@@ -1,21 +1,42 @@
-const Discord = require('discord.js');
-const data = require('quick.db');
+const Discord = require('discord.js')
+const db = require("quick.db");
+const database = require('quick.db');
 
-exports.run = async (client, message, args) => {
-const DBL = require('dblapi.js')
+
+exports.run = async(client, message, args) => {
+ const DBL = require('dblapi.js')
 const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1Njg4MzMwOTI3MDY2MzIyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjU2MTEwOTkxfQ.7Oqg1lelprL5ACm4Yh0RKREKaOTPIyQRrSjDaT7uKko', client)
 dbl.hasVoted(message.author.id).then(voted => {
-      if(voted) {   
-  
-  const ayarlar = require('../ayarlar.json')
-const db = require('quick.db')
-let prefix = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix;
-  if(!message.member.permissions.has('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().setThumbnail(message.author.avatarURL() ? message.author.avatarURL({dynamic: true}) : 'https://cdn.glitch.global/d80f45da-14d1-42a7-a16d-8f3385ce86a4/c10f8dedd93c798ce1e36ad9acb26fd4.png?v=1657994146595').setImage('https://cdn.glitch.com/0c8ef551-5187-48a8-9daf-f2cc35630f21%2Fyoneticigif.gif').setTitle('Bir hata oldu!').setDescription(`• \`${prefix}jail-yetkili-rol\` **kullanmak için,** \`Yönetici\` **yetkisine sahip olman gerekiyor.**`));
-  if(!message.mentions.roles.first()) return message.channel.send(new Discord.MessageEmbed().setColor('#00001').setTitle('Bir hata oldu!').setDescription('Bir rol etiketlemeyi unuttunuz.'));
-  let mentionRole = message.mentions.roles.first();
-  data.set(`jail.yetkilirole.${message.guild.id}`, mentionRole.id);
-  message.channel.send(new Discord.MessageEmbed().setTitle('İşte bu kadar!').setDescription(`Jail sistemin de kullanılacak: ${mentionRole} olarak seçtiniz.`));
+      if(voted) {  
+  let yrol =  database.fetch(`yrol.${message.guild.id}`)
+  //if(!yrol) return message.channel.send(`Yetkili rolü ayarlanmamış!`)
+//  if(!message.member.roles.cache.has(yrol)) return message.channel.send(`Bu komutu kullanabilmek için YETKİLİ ROLÜNE sahip olmalısın.`)
+   var başarılı = ['**İŞTE BU!** <a:tik1:823900091143159828>', '**SÜPER!** <a:tik1:823900091143159828>', '**NASIL YAPTIN BUNU?!** <a:tik1:823900091143159828>', '**MÜKEMMEL!** <a:tik1:823900091143159828>', '**SEVDİM BUNU!** <a:tik1:823900091143159828>', '**ŞİMDİ OLDU!** <a:tik1:823900091143159828>'];
+   var x = başarılı[Math.floor(Math.random() * başarılı.length)];
 
+   var başarısız = ['**TÜH!** <a:alarm3:823900153831620658>', '**OLMADI BU!** <a:alarm3:823900153831620658>', '**HAY AKSİ!** <a:alarm3:823900153831620658>', '**HADİ ORADAN!** <a:alarm3:823900153831620658>', '**OLMADI YA!** <a:alarm3:823900153831620658>', '**BÖYLE OLMAZ?!** <a:alarm3:823900153831620658>', '**HADİ YA!** <a:alarm3:823900153831620658>'];
+   var x2 = başarısız[Math.floor(Math.random() * başarısız.length)];
+  
+if (!message.member.hasPermission('MANAGE_GUILD')) return message.reply(`**s!jail-yetkilisi ayarla/sıfırla** isimli komutu kullanabilmek için \`SUNUCUYU YÖNET\` yetkisine sahip olman gerekiyor.`)
+if (!args[0]) return message.reply(`Sistemi kullanabilmek için, !sjail-yetkilisi ayarla/sıfırla @rol yazmalısın.`)
+   
+  
+  if (args[0] == 'ayarla') {
+  
+  let yetkilirol = message.mentions.roles.first() || message.guild.roles.find(c => c.name === args[1].join(' '))
+  if (!yetkilirol) return message.channel.send(x2 + ` Bir rol etiketle.`)
+  
+  db.set(`jailyetkilisi_${message.guild.id}`, yetkilirol.id)
+  message.channel.send(x + ` Jail yetkilisi ${yetkilirol} olarak ayarlandı.`)
+  } 
+  
+
+  if (args[0] == 'sıfırla') {
+    db.delete(`jailyetkilisi_${message.guild.id}`)
+    message.channel.send(x + ` Jail yetkilisi başarıyla sıfırlandı.`)
+  }
+  
+  
 } else {
         message.channel.send(` Bu Komutu Sadece 12 Saatte Bir Oyvererek Kullanabilirsiniz Oyvermek İçin (https://top.gg/bot/756883309270663229/vote) linke Tıklayarak Oyverebilirsiniz. Oy Verdiyseniz 5 Dakka Bekleyiniz`) 
               
@@ -24,12 +45,14 @@ let prefix = db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix;
       
       },
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: [],
-  permLevel: 0
-}
+ enabled: true,
+ guildOnly: false,
+   aliases: ['jailyetkilisi'],
+ permLevel: 0
+};
 
 exports.help = {
-  name: 'jail-yetkili-rol'
+ name: 'jail-yetkilisi',
+ description: 'Hangi role sahip kişilerin jaile atabileceğini ayarlarsınız.',
+ usage: 'jail-yetkilisi ayarla/sıfırla @rol'
 };
