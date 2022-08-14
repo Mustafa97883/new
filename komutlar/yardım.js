2c6521a31569b17ad027afa87f5e0b1b783b74fa
Glitch (cooperative-spiffy-dead) <none>
2022-08-14 23:40:25 +0000

    🍜👏 Checkpoint ./komutlar/yardım.js:14029226/84

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,7 +6,7 @@ let cse = new Discord.MessageEmbed()//discord.gg/turkiye
 .setThumbnail(client.user.avatarURL())
 .setDescription(`[OYVER](https://top.gg/bot/756883309270663229/vote)
 **Strom Müzik botunu eklemek için [TIKLA](https://discord.com/api/oauth2/authorize?client_id=854122011151826975&permissions=8&scope=bot%20applications.commands)
-**
+**Onay sürecinde olduğu için bazı komutlar çalışamaz!**
 
 • **tag** \`Destek sunucusunun tagını gösterir.\`\n
 • **s!moderasyon-sistemi**  \`Moderasyon komutlarını gösterir.\` \n
