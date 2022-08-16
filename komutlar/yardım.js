2f6bbdea337a0f14fc11f9263782904def6fc458
Glitch (cooperative-spiffy-dead) <none>
2022-08-16 16:25:48 +0000

    Rewound to commit 1c2084cf33c0adcf823cd742503b12f55cb5df2f + 0 edits

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -18,6 +18,7 @@ let cse = new Discord.MessageEmbed()//discord.gg/turkiye
 • **s!eğlence-yardım**        \`Eğlence komutlarını gösterir.\` \n
 • **s!jail-yardım**      \`jail komutlarını gösterir.\` \n
 • **s!kullanıcı-yardım**      \`kullanıcı komutlarını gösterir.\` \n
+• **s!nsfw-yardım**     \`+18 komutları gösterir.\` \n
 • **s!çekiliş-sistemi**      \`çekiliş komutlarını gösterir.\` \n
 • **s!yardım-botlist**     \`Botlist komutlarını gösterir.\` \n
 
