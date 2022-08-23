6bb5a4893fe20b5aadb82de840b799bb6102c162
Glitch (cooperative-spiffy-dead) <none>
2022-08-23 13:29:32 +0000

    🏈🚉 Checkpoint ./events/message.js:14029226/197 ./events/ready.js:14029226/22

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@
 const ayarlar = require('../ayarlar.json');
 
 module.exports = async client => {
-  var oyun = ["🔥s!yardım🔥","🔥Strom, sunucunuz için🔥","🔥s!oyver🔥","🔥En gelişmiş komutlar🔥","🔥s!güncellemeler🔥","🔥s!guard🔥","⚠️Herhangi bir hatada s!hata⚠️"];
+  var oyun = ["🔥s!yardım🔥","🔥Strom, sunucunuz için🔥","🔥s!oyver🔥","🔥En gelişmiş komutlar🔥","🔥s!guard🔥","⚠️Herhangi bir hatada s!hata⚠️"];
 
   setInterval(async () => {
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
